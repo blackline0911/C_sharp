28a313571b47bcaf302f10e4867236996c1ba92d
kevin <kevin891104@gmail.com>
2025-04-18 11:18:30 +0800

    Beginning

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//namespace ConsoleApp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,\n\t World\n\n !");
+        }
+        
+    }
+}
